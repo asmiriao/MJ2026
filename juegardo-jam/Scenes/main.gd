f8d322ec0e85193f89cb49979bb774714d181fbd
Federico García <fedgartal@gmail.com>
2026-01-31 23:56:11 +0100

    si
    va cogiendo forma

--- juegardo-jam/Scenes/main.gd
+++ juegardo-jam/Scenes/main.gd
@@ -1,9 +1,9 @@
 extends Node3D
-
+const ALMA_1 = preload("uid://cxi16kb2wu3h4")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	DialogueManager.show_dialogue_balloon(ALMA_1, "dialogoAlma1")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- juegardo-jam/Scripts/game_manager.gd
+++ juegardo-jam/Scripts/game_manager.gd
@@ -1,13 +1,18 @@
 extends Node
 const ALMA_1 = preload("uid://cxi16kb2wu3h4")
 
+@onready var alma: CharacterBody3D = $Alma
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	DialogueManager.show_dialogue_balloon(ALMA_1, "dialogoAlma1")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func enter_shadow():
-	print("Shadow entered")
+	await get_tree().create_timer(2).timeout
+	DialogueManager.show_dialogue_balloon(ALMA_1, "dialogoAlma1")
