f7ee502b5da1fa60761964adbd27ab4aafad769c
Federico García <fedgartal@gmail.com>
2026-02-01 09:17:09 +0100

    xd

--- juegardo-jam/Scripts/game_manager.gd
+++ juegardo-jam/Scripts/game_manager.gd
@@ -4,6 +4,7 @@ const ALMA_1 = preload("uid://cxi16kb2wu3h4")
 @onready var alma: CharacterBody3D = $Alma
 
 var activado = false
+var almas = preload("res://Scenes/alma.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,8 +15,11 @@ func _process(delta: float) -> void:
 	pass
 
 func enter_shadow():
+	 
 	if not activado:
 		activado = true
 		await get_tree().create_timer(2).timeout
 		DialogueManager.show_dialogue_balloon(ALMA_1, "dialogoAlma1")
 		
+func shadow_disappear():
+	pass
