fcae9bb88adf5d641bde8f29c0eff1224c364493
Santy <161971086+popieyes@users.noreply.github.com>
2026-03-09 12:24:29 +0100

    Bug Fix: Volume sliders and volume value changing

--- juegardo-jam/UI/options_menu/VolumeControllerClass.gd
+++ juegardo-jam/UI/options_menu/VolumeControllerClass.gd
@@ -10,15 +10,14 @@ func _ready() -> void:
 	init_audio()
 
 func _on_music_slider_value_changed(value : float) -> void:
-	AudioServer.set_bus_volume_db(_music_bus_idx, music_slider.value)
-	pass # Replace with function body.
+	# Convert audio to a logarithmic scale (That's how volume works)
+	AudioServer.set_bus_volume_db(_music_bus_idx, linear_to_db(value))
 
 func _on_sfx_slider_value_changed(value : float) -> void:
-	AudioServer.set_bus_volume_db(_sfx_bus_idx, sfx_slider.value)
-	pass # Replace with function body.
+	# Convert audio to a logarithmic scale (That's how volume works)
+	AudioServer.set_bus_volume_db(_sfx_bus_idx, linear_to_db(value))
 
 func init_audio() -> void:
-	music_slider.value = -15
-	sfx_slider.value = -15
-	AudioServer.set_bus_volume_db(_music_bus_idx, music_slider.value)
-	AudioServer.set_bus_volume_db(_sfx_bus_idx, sfx_slider.value)
+	# Get current volume of bus and set it to the slider (convert from log to linear scale)
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(_music_bus_idx))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(_sfx_bus_idx))
